cee1b1370ba3cc9d440e53bbfa4a3c50c5a2b00b
ritzyRosabel <rosabelolugbenga@yahoo.com>
2020-07-15 22:32:07 +0100

    Adding Razor runtime compiation. This helps to  automatically reompile projects when changes are made to the razor views

--- BookListMVC/Startup.cs
+++ BookListMVC/Startup.cs
@@ -26,7 +26,7 @@ namespace BookListMVC
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
-                services.AddControllersWithViews();
+                services.AddControllersWithViews().AddRazorRuntimeCompilation();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
