e9e43245eb2408c6a9334ae3f59976b21bd71196
ritzyRosabel <rosabelolugbenga@yahoo.com>
2020-07-15 23:13:04 +0100

    Get all books Action method

--- BookListMVC/Controllers/BooksController.cs
+++ BookListMVC/Controllers/BooksController.cs
@@ -2,15 +2,28 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BookListMVC.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BookListMVC.Controllers
 {
     public class BooksController : Controller
     {
+        private readonly ApplicationDbContext _db;
+        public BooksController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpGet]
+        [Route("api/Book")]
+        public IActionResult GetAll()
+        {
+            return Json(new { data = _db.Books.ToList() });
         }
     }
 }
